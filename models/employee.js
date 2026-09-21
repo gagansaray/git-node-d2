5e59125a84911e09fd535464d0e293e194e72ae0
Gagandeep <singhsaray@gmail.com>
2026-09-21 13:26:40 -0400

    Updated employee model

--- models/employee.js
+++ models/employee.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.connect(
-  'mongodb://localhost:27017/employees
+  'mongodb://localhost:27017/employees'
 ).then(() => {
   console.log('MongoDB connected successfully');
 }).catch((err) => {
